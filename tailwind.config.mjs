d9ebca359c685bf3aef0347fd302e897bbeee2af
Adam King <adamking@GenZen-Air.local>
2025-04-18 09:20:20 +0400

    Fix CSS configuration and dependencies

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -61,26 +61,6 @@ export default {
           950: '#3a1725',
         },
       },
-      animation: {
-        marquee: 'marquee 12s linear infinite',
-        rightMarquee: 'rightMarquee 300s linear infinite',
-        slowMarquee: 'marquee 300s linear infinite',
-        verticalMarquee: 'vertical 30s linear infinite',
-      },
-      keyframes: {
-        marquee: {
-          '0%': { transform: 'translateX(0%)' },
-          '100%': { transform: 'translateX(-100%)' },
-        },
-        rightMarquee: {
-          '0%': { transform: 'translateX(0%)' },
-          '100%': { transform: 'translateX(100%)' },
-        },
-        vertical: {
-          '0%': { transform: 'translateY(0%)' },
-          '100%': { transform: 'translateY(-100%)' },
-        },
-      },
     },
   },
   plugins: [
